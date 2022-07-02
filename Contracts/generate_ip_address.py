8e760ffa37bbf853088a608ff60011c1ee18adf7
Odd Mathisen <odda2k@hotmail.com>
2022-07-02 20:58:21 +0200

    🚧 Only problem text added

---
+++ Contracts/generate_ip_address.py
@@ -0,0 +1,18 @@
+# Generate IP Addresses
+#
+# You are attempting to solve a Coding Contract. You have 10 tries remaining,
+# after which the contract will self-destruct.
+#
+# Given the following string containing only digits, return an array with all
+# possible valid IP address combinations that can be created from the string:
+#
+# 3324516100
+#
+# Note that an octet cannot begin with a '0' unless the number itself is
+# actually 0. For example, '192.168.010.1' is not a valid IP.
+#
+# Examples:
+#
+# 25525511135 -> [255.255.11.135, 255.255.111.35] 1938718066 -> [193.87.180.66]
+
+# TODO: Implement the algorithm.
