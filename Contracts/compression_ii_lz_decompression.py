58444810fc9b2c69c9989d30e3906e8d06e639bc
Odd Mathisen <odda2k@hotmail.com>
2022-07-03 18:05:20 +0200

    ✨ Working LZ decompression Debugging, yay ;)

--- Contracts/compression_ii_lz_decompression.py
+++ Contracts/compression_ii_lz_decompression.py
@@ -32,12 +32,26 @@
 #     5aaabb450723abb  ->  aaabbaaababababaabb
 
 
-# TODO: Not working on main input string, works on testing input.
-#   - I completely misunderstood the problem...
+from itertools import islice
+from typing import Callable
+
+DEBUGGING = False
+
+
+def debug(*args, func: Callable = None) -> None:
+    """Prints a message if debugging is enabled
+    Args:
+        message (str): Message to print
+    """
+    if DEBUGGING:
+        if func:
+            func(*args)
+        else:
+            print(*args)
 
 
 def main():
-    testing = True
+    testing = False
     if testing:
         input_pairs = [
             ("5aaabb", "aaabb"),
@@ -66,7 +80,7 @@ def main():
         print(f"Decompressed string: {decompress(input_string)}")
 
 
-def decompress(input_string: str) -> str:
+def decompress(input_string: str) -> str | None:
     """Decompress a LZ-encoded string
     Args:
         input_string (str): LZ-encoded string
@@ -74,42 +88,58 @@ def decompress(input_string: str) -> str:
         str: Decompressed string
     """
     output_string = ""
-    given_length = int(input_string[0])
-    if given_length == 0:
-        return output_string
-    data = input_string[1:]
-    if given_length == len(data):
-        return data
-    else:
-        output_string += data[:given_length]
-        data = data[given_length:]
-        print(f"Base Output string: {output_string}")
-        print(f"Base Data: {data}")
-        while len(data) > 0:
-            given_length = int(data[0])
-            print(f"Given length: {given_length}")
-            print(f"Data: {data}")
-            data = data[1:]
-            if given_length == 0:
+    chunk_type = 1
+    chunks = iter(enumerate(input_string))
+    for i, character in chunks:
+        # Incorrect input errors
+        if not character.isdigit():
+            debug(f"Invalid character: {character}")
+            return None
+        # Done with final chunk
+        if i >= len(input_string):
+            debug("Done with final chunk")
+            break
+
+        if chunk_type == 1:
+            debug(f"Processing type 1 chunk, character {character}")
+            if int(character) + i >= len(input_string):
+                debug(f"Invalid chunk length for type 1: {character}")
+                return None
+            # 0 is already handled by character, so can skip that logic here
+            output_string += input_string[i + 1 : int(character) + i + 1]
+            debug(f"\tOutput string: {output_string}")
+            # Skip to the next chunk (skipping with the value of character)
+            next(islice(chunks, int(character), int(character)), None)
+            chunk_type = 2
+        elif chunk_type == 2:
+            distance = int(character)
+            debug(f"Processing type 2 chunk, distance {distance}")
+            if distance == 0:
+                # Skip to the next chunk (0 character)
+                chunk_type = 1
                 continue
-            if data[0].isalpha() and given_length == len(data):
-                output_string += data
-                data = ""
             else:
-                backtrack = int(data[0])
-                print(f"Backtrack: {backtrack}")
-                data = data[1:]
-                while given_length > backtrack:
-                    output_string += output_string[-backtrack:]
-                    given_length -= backtrack
-                print(
-                    f"Output string: {output_string} - Given length: {given_length} - backtrack: {backtrack}"
-                )
-                output_string += output_string[-backtrack : -(backtrack - given_length)]
-                print(
-                    f"Trunctuated Output string: {output_string[-backtrack:-(backtrack-given_length)]}"
-                )
-        return output_string
+                # Incorrect input error
+                if i + 1 >= len(input_string):
+                    debug(
+                        f"Reached end of input string, i+1 is: {i+1}, length is: {len(input_string)}distance is: {distance}"
+                    )
+                    return None
+                if not input_string[i + 1].isdigit():
+                    debug(f"Invalid character: {input_string[i + 1]}")
+                    return None
+                offset = int(input_string[i + 1])
+                debug(f"\tOffset is: {offset}")
+                if offset > len(output_string):
+                    debug(f"Invalid offset: {offset}")
+                    return None
+                for i in range(distance):
+                    output_string += output_string[len(output_string) - offset]
+                debug(f"\tOutput string: {output_string}")
+                # Skip to the next chunk (1 character)
+                next(islice(chunks, 1, 1), None)
+            chunk_type = 1
+    return output_string
 
 
 if __name__ == "__main__":
