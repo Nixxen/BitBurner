a5d68dc2e7ca61ce44cd556de7da104ce5a41ab8
Odd Mathisen <odda2k@hotmail.com>
2022-07-10 00:58:48 +0200

    ✨ Added moneyKeep logic And refactored to const, let, and arrow functions

--- Playground/scripts/early/purchase-servers.js
+++ Playground/scripts/early/purchase-servers.js
@@ -1,73 +1,84 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	var target = ns.args[0];
-	var homeServ = "home";
-	var pRam = 8; // purchased ram
-	var servPrefix = "pserv-";
+	const target = ns.args[0];
+	const homeServ = "home";
+	let pRam = 8; // purchased ram
+	const servPrefix = "pserv-";
+	const moneyKeepLimit = 15 * 10 ** 6; // 15m - Adjust this lower if you do not have enough yet
+	const sleepAwaitTime = 10000;
 
-	var maxRam = ns.getPurchasedServerMaxRam();
-	var maxServers = ns.getPurchasedServerLimit();
+	const maxRam = ns.getPurchasedServerMaxRam();
+	const maxServers = ns.getPurchasedServerLimit();
 
-	var virus = "/scripts/early/money-maker.js";
-	var virusRam = ns.getScriptRam(virus);
+	const virus = "/scripts/early/money-maker.js";
+	const virusRam = ns.getScriptRam(virus);
 
-	function canPurchaseServer() {
+	const playerCanAfford = (cost) => {
+		return getPlayerMoney() - cost > moneyKeepLimit;
+	};
+
+	const getPlayerMoney = () => ns.getServerMoneyAvailable(homeServ);
+
+	const canPurchaseServer = () => {
 		ns.disableLog("disableLog");
 		ns.disableLog("getServerMoneyAvailable");
-		var playerMoney = ns.getServerMoneyAvailable(homeServ);
-		var serverCost = ns.getPurchasedServerCost(pRam);
+		const playerMoney = getPlayerMoney();
+		const serverCost = ns.getPurchasedServerCost(pRam);
 		ns.print(
-			"Awaiting player money to increase: " +
-				ns.nFormat(playerMoney, "($ 0.00 a)") +
-				"/" +
-				ns.nFormat(serverCost, "($ 0.00 a)") +
-				"(" +
-				((playerMoney / serverCost) * 100).toFixed(2) +
-				"%)"
+			`\tAwaiting player money to increase: ${ns.nFormat(
+				playerMoney,
+				"$0.00a"
+			)}/${ns.nFormat(serverCost + moneyKeepLimit, "$0.00a")}(${(
+				(playerMoney / serverCost) *
+				100
+			).toFixed(2)}%) (with ${ns.nFormat(
+				moneyKeepLimit,
+				"0.00a"
+			)} offset).`
 		);
-		return playerMoney > serverCost;
-	}
+		return playerCanAfford(serverCost);
+	};
 
-	function killVirus(server) {
+	const killVirus = (server) => {
 		if (ns.scriptRunning(virus, server)) {
 			ns.scriptKill(virus, server);
 		}
-	}
+	};
 
-	async function copyAndRunVirus(server) {
+	const copyAndRunVirus = async (server) => {
 		ns.print("Copying virus to server: " + server);
 		await ns.scp(virus, server);
 		killVirus(server);
 		var maxThreads = Math.floor(pRam / virusRam);
 		ns.exec(virus, server, maxThreads, target);
-	}
+	};
 
-	function shutdownServer(server) {
+	const shutdownServer = (server) => {
 		killVirus(server); // need to stop scripts before delete
 		ns.deleteServer(server);
-	}
+	};
 
-	async function upgradeServer(server) {
+	const upgradeServer = async (server) => {
 		var sRam = ns.getServerMaxRam(server);
 		if (sRam < pRam) {
 			while (!canPurchaseServer()) {
-				await ns.sleep(10000);
+				await ns.sleep(sleepAwaitTime);
 			}
 			shutdownServer(server);
 			ns.purchaseServer(server, pRam);
 		}
 		await copyAndRunVirus(server);
-	}
+	};
 
-	async function purchaseServer(server) {
+	const purchaseServer = async (server) => {
 		while (!canPurchaseServer()) {
-			await ns.sleep(10000);
+			await ns.sleep(sleepAwaitTime);
 		}
 		ns.purchaseServer(server, pRam);
 		await copyAndRunVirus(server);
-	}
+	};
 
-	async function autoUpgradeServers() {
+	const autoUpgradeServers = async () => {
 		var i = 0;
 		while (i < maxServers) {
 			var server = servPrefix + i;
@@ -81,7 +92,7 @@ export async function main(ns) {
 				++i;
 			}
 		}
-	}
+	};
 
 	while (true) {
 		await autoUpgradeServers();
