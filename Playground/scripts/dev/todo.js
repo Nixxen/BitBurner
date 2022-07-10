5909aed59ddc79d75177df3da073d24805737f69
Odd Mathisen <odda2k@hotmail.com>
2022-07-10 22:38:11 +0200

    💡 Reminder comments for my own sanity

---
+++ Playground/scripts/dev/todo.js
@@ -0,0 +1,39 @@
+// TODO:
+// - [ ] Make backdoor deploy script for any known servers, based on previous runs (faction invites, cost decreases, etc)
+//     - Servers:
+//         - [ ] CSEC (CSEC faction)
+//         - [ ] avmnite-02h (NiteSec faction)
+//         - [ ] I.I.I.I (The Black Hand faction)
+//         - [ ] iron-gym (Cheaper workout cost)
+//         - [ ] run4theh111z (Bitrunners faction, requires deep scanning > 10 hops)
+//      - Edit: Turns out I can not backdoor servers through scripts yet, and that is apparently something that is unlocked later...
+// - [ ] Make fleet hack script
+// - [ ] Make "get server path" helper script
+// - [ ] Make a script to scan for, and solve contracts (if possible)
+
+// Completed factions:
+// - [x] CSEC (Hacking.)
+//      - No faction lock
+// - [x] NiteSec (Hacking.)
+//      - Faction lock: Hacking exp and skill
+// - [x] The Black Hand (Hacking.)
+//      - Faction lock: Hacking, dex, and str skill, hack power, hack speed
+// - [x] Bitrunners (Hacking.)
+//      - Faction lock: Hacking, dex, and str skill, hack power, hack speed
+//      - Start with FTPCrack and relaySMTP
+// - [x] Four Sigma (Job/Work related + hacking + faction/rep boosts etc.)
+//      - No faction lock
+// - [x] Tian Di Hui (Job/Work related + faction/rep boosts etc.)
+//      - Faction lock: Rep + money from work. Removes focus penalty from tasks.
+// - [x] Netburners (Hacknet nodes)
+//      - Faction lock: Hacknet related
+// - [ ] Chongquin
+// - [x] Sector-12 (??)
+//      - Faction lock: ??
+// - [ ] Aevum
+// - [ ] Volkhaven
+// - [ ] New Tokyo (Mixed bag.)
+//      - Faction lock: +20% combat exp
+// - [ ] Ishima (Combat skill related)
+//      - Faction lock: Dex skill, crime money and crime success rate
+// - [ ] NSA?
