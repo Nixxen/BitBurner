9d21286153787336e3b5fb2f9e6d4b13001632c3
Odd Mathisen <odda2k@hotmail.com>
2022-07-10 05:49:58 +0200

    🩹 Scope variable fix

--- Playground/scripts/early/find-server.js
+++ Playground/scripts/early/find-server.js
@@ -1,43 +1,43 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-	var target = ns.args[0];
-	var origin = ns.getHostname();
+	const target = ns.args[0];
+	const origin = ns.getHostname();
 
 	// patterns that indicates whether we ignore them
-	var ignored = [
-		"pserv"
-	]
+	const ignored = ["pserv"];
 
-	function hasIgnoredString(text) {
-		return ignored.some(function (str) { return text.includes(str) });
-	}
+	const hasIgnoredString = (text) => {
+		return ignored.some(function (str) {
+			return text.includes(str);
+		});
+	};
 
 	// Use DFS to find visit all nodes in the network until we find
 	// the target server
 	function getNetworkNodePairs() {
-		var visited = {};
-		var stack = [];
+		let visited = {};
+		let stack = [];
 		stack.push(origin);
-		var nodePairs = [];
+		let nodePairs = [];
 
 		while (stack.length > 0) {
-			var node = stack.pop();
+			const node = stack.pop();
 			if (!visited[node]) {
 				if (node === target) {
 					break;
 				}
 				visited[node] = node;
-				var neighbours = ns.scan(node);
-				for (var i = 0; i < neighbours.length; i++) {
-					var child = neighbours[i];
+				const neighbours = ns.scan(node);
+				for (let i = 0; i < neighbours.length; i++) {
+					const child = neighbours[i];
 					if (hasIgnoredString(child) || visited[child]) {
 						continue;
 					}
 					stack.push(child);
-					var pair = {
+					const pair = {
 						parent: node,
-						current: child
-					}
+						current: child,
+					};
 					nodePairs.push(pair);
 				}
 			}
@@ -47,15 +47,15 @@ export async function main(ns) {
 
 	function reconstructPath(nodes) {
 		// for every node, map them to parent
-		var parentMap = nodes.reduce(function (acc, node) {
+		const parentMap = nodes.reduce(function (acc, node) {
 			acc[node.current] = node.parent;
 			return acc;
 		}, {});
 
 		ns.print("Target found. Recreating path");
 		ns.print("Number of nodes tracked: " + nodes.length);
-		var path = [];
-		var curNode = target;
+		let path = [];
+		let curNode = target;
 		while (curNode !== origin) {
 			path.push(curNode);
 			ns.print("Adding server to path: " + curNode);
@@ -69,7 +69,7 @@ export async function main(ns) {
 		return path.reverse();
 	}
 
-	var nodes = getNetworkNodePairs();
-	var path = reconstructPath(nodes);
+	const nodes = getNetworkNodePairs();
+	const path = reconstructPath(nodes);
 	ns.tprint(path);
 }
