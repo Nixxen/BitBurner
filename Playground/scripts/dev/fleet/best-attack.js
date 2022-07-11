bde7918b4ae07d3046a4e07ffd9dc62a789b10b4
Odd Mathisen <odda2k@hotmail.com>
2022-07-12 00:43:43 +0200

    ✨ getNetworkNodes and canHack functions

--- Playground/scripts/dev/fleet/best-attack.js
+++ Playground/scripts/dev/fleet/best-attack.js
@@ -1,10 +1,18 @@
+import { getNetworkNodes, canHack } from "../util";
+
 /** @param {NS} ns */
 export async function main(ns) {
 	// Scans all servers and finds the most profitable, single attack, server.
 	// This is only profitable for single attacks, not multiple servers attacking the same server.
 	// Logic:
 	// - Get all servers on the network (DFS)
-	// - For each server, filter for hackable (based on current hack skill)
+	// - For each server, filter for hackable (based on current hack skill, ignoring pserv)
 	// - For each hackable server, get server info
 	// - Sort servers by profit (max money? Does hacking level affect this? Other factors?)
-    // - Return the most profitable server (or save the list of all servers for later)
+	// - Return the most profitable server (or save the list of all servers for later)
+
+	const networkNodes = getNetworkNodes(ns);
+	const hackableServers = networkNodes.filter(
+		(node) => !node.includes("pserv") && canHack(ns, node)
+	);
+}
--- Playground/scripts/dev/util.js
+++ Playground/scripts/dev/util.js
@@ -1,12 +1,55 @@
-/** @param {NS} ns */
-// Range generator. Use in for ... of loops
-export function* range(start=0, end=null, step=1) {
+/**
+ * Range generator. Use in for ... of loops
+ * @param {NS} ns
+ */
+export function* range(start = 0, end = null, step = 1) {
 	if (end == null) {
 		end = start;
 		start = 0;
 	}
 
-	for (let i=start; i < end; i+=step) {
+	for (let i = start; i < end; i += step) {
 		yield i;
 	}
+}
+
+/**
+ * Returns a list of all nodes in the network using DFS
+ * @remarks RAM cost: 0.2GB
+ * @param {NS} ns
+ * @returns {string[]} List of all nodes in the network
+ */
+export function getNetworkNodes(ns) {
+	let visited = {};
+	let stack = [];
+	stack.push(origin);
+	let nodes = [];
+	while (stack.length > 0) {
+		const node = stack.pop();
+		if (!visited[node]) {
+			visited[node] = node;
+			const neighbors = ns.scan(node);
+			for (const neighbor of neighbors) {
+				if (visited[neighbor]) {
+					continue;
+				}
+				stack.push(neighbor);
+				nodes.push(neighbor);
+			}
+		}
+	}
+	return nodes;
+}
+
+/**
+ * Checks if a server can be hacked by the player
+ * @remarks RAM cost 0.15GB
+ * @param {NS} ns
+ * @param {string} node server to check
+ * @returns {boolean} true if server can be hacked
+ */
+export function canHack(ns, node) {
+	const hackSkill = ns.getHackingLevel();
+	const nodeHackSkill = ns.getServerRequiredHackingLevel(node);
+	return hackSkill >= nodeHackSkill;
 }
