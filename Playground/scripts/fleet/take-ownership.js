0c60999475bb15b717e47b2f9e6af99f146dbac0
Odd Mathisen <odda2k@hotmail.com>
2022-08-02 12:41:30 +0200

    ♻️ Made extractable function + cleanup

--- Playground/scripts/fleet/take-ownership.js
+++ Playground/scripts/fleet/take-ownership.js
@@ -1,8 +1,10 @@
-/** @param {NS} ns **/
-export async function main(ns) {
-	var server = ns.args[0];
-	var homeServer = "home";
-	var cracks = {
+/**
+ * Attempts to take ownership of the targeted server.
+ *  @param {NS} ns
+ **/
+export async function takeOwnership(ns, server) {
+	const homeServer = "home";
+	const cracks = {
 		"BruteSSH.exe": ns.brutessh,
 		"FTPCrack.exe": ns.ftpcrack,
 		"relaySMTP.exe": ns.relaysmtp,
@@ -10,17 +12,11 @@ export async function main(ns) {
 		"SQLInject.exe": ns.sqlinject,
 	};
 
-	function getNumCracks() {
-		return Object.keys(cracks).filter(function (file) {
-			return ns.fileExists(file, homeServer);
-		}).length;
-	}
-
 	function penetrate(server) {
 		ns.tprint("Penetrating " + server);
 		for (var file of Object.keys(cracks)) {
 			if (ns.fileExists(file, homeServer)) {
-				var runScript = cracks[file];
+				const runScript = cracks[file];
 				runScript(server);
 			}
 		}
@@ -28,17 +24,22 @@ export async function main(ns) {
 
 	async function gainAccess(server) {
 		if (!ns.hasRootAccess(server)) {
-			var requiredPorts = ns.getServerNumPortsRequired(server);
+			const requiredPorts = ns.getServerNumPortsRequired(server);
 			if (requiredPorts > 0) {
 				penetrate(server);
 			}
-			ns.tprint("Gaining root access on " + server);
+			ns.tprint("Server penetrated. Gaining root access on " + server);
 			ns.nuke(server);
 		} else {
 			ns.tprint("Already have root access on " + server);
 		}
 	}
 
-	var banana = await gainAccess(server);
-	ns.print(banana);
+	await gainAccess(server);
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	var server = ns.args[0];
+	takeOwnership(ns, server);
 }
