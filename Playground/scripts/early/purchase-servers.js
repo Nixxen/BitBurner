690454cdfca549bb40632066ee715594ac5712d5
Odd Mathisen <odda2k@hotmail.com>
2022-08-06 21:27:58 +0200

    ✨ Purchase limit on purchase server

--- Playground/scripts/early/purchase-servers.js
+++ Playground/scripts/early/purchase-servers.js
@@ -6,6 +6,11 @@ export async function main(ns) {
 	ns.disableLog("getServerMoneyAvailable");
 
 	const target = ns.args[0];
+	if (!target) {
+		ns.tprint("No target specified. Exiting.");
+		return;
+	}
+	const purchaseLimit = parseInt(ns.args[1]) || 0; // Max price to pay for next upgrade. 0 is disabled
 	const homeServ = "home";
 	let pRam = 8; // purchased ram
 	const servPrefix = "pserv-";
@@ -24,6 +29,10 @@ export async function main(ns) {
 
 	const getPlayerMoney = () => ns.getServerMoneyAvailable(homeServ);
 
+	const isUnderPurchaseLimit = (price) => {
+		return !purchaseLimit || price <= purchaseLimit;
+	};
+
 	const canPurchaseServer = () => {
 		const playerMoney = getPlayerMoney();
 		const serverCost = ns.getPurchasedServerCost(pRam);
@@ -39,7 +48,7 @@ export async function main(ns) {
 				"0.00a"
 			)} offset).`
 		);
-		return playerCanAfford(serverCost);
+		return playerCanAfford(serverCost) && isUnderPurchaseLimit(serverCost);
 	};
 
 	const killVirus = (server) => {
@@ -77,25 +86,65 @@ export async function main(ns) {
 		while (!canPurchaseServer()) {
 			await ns.sleep(sleepAwaitTime);
 		}
-		ns.purchaseServer(server, pRam);
+		const result = ns.purchaseServer(server, pRam);
+		if (!result) {
+			ns.tprint(
+				`Failed to purchase server: ${server} with ram/max ${pRam}/${ns.getPurchasedServerMaxRam()} and cost/player ${ns.nFormat(
+					ns.getPurchasedServerCost(pRam),
+					"$0.000a"
+				)}/${ns.nFormat(
+					getPlayerMoney(),
+					"$0.000a"
+				)}. Debug through the servers.js script.`
+			);
+		}
 		await copyAndRunVirus(server);
 	};
 
 	const autoUpgradeServers = async () => {
-		var i = 0;
-		while (i < maxServers) {
-			var server = servPrefix + i;
-			if (ns.serverExists(server)) {
-				ns.print("Upgrading server " + server + " to " + pRam + "GB");
-				await upgradeServer(server);
-				++i;
-			} else {
-				ns.print("Purchasing server " + server + " at " + pRam + "GB");
-				await purchaseServer(server);
-				++i;
+		let i = 0;
+		let cost = ns.getPurchasedServerCost(pRam);
+		while (i < maxServers && isUnderPurchaseLimit(cost)) {
+			let server = servPrefix + i;
+			let cost = ns.getPurchasedServerCost(pRam);
+			if (isUnderPurchaseLimit(cost)) {
+				if (ns.serverExists(server)) {
+					ns.print(
+						"Upgrading server " + server + " to " + pRam + "GB"
+					);
+					await upgradeServer(server);
+					++i;
+				} else {
+					ns.print(
+						"Purchasing server " +
+							server +
+							" for " +
+							ns.nFormat(cost, "$0.000a") +
+							"."
+					);
+					await purchaseServer(server);
+					++i;
+				}
 			}
 		}
 	};
+
+	const limitText =
+		purchaseLimit == 0
+			? "<Unlimited>"
+			: ns.nFormat(purchaseLimit, "$0.000a");
+	ns.tprint(
+		"Purchasing and upgrading servers named " +
+			servPrefix +
+			"0 to " +
+			servPrefix +
+			(maxServers - 1) +
+			". Lower money threshold is " +
+			ns.nFormat(moneyKeepLimit, "$0.000a") +
+			". Upper purchase limit is " +
+			limitText +
+			"."
+	);
 
 	while (true) {
 		await autoUpgradeServers();
@@ -105,12 +154,21 @@ export async function main(ns) {
 			);
 			break;
 		}
-		// move up to next tier
-		var newRam = pRam * 2;
-		if (newRam > maxRam) {
-			pRam = maxRam;
+		// check if cost is too high
+		const cost = ns.getPurchasedServerCost(pRam);
+		if (!isUnderPurchaseLimit(cost)) {
+			ns.tprint(
+				`Next server purchase or upgrade is above purchase limit. Aborting script.`
+			);
+			break;
 		} else {
-			pRam = newRam;
+			// move up to next tier
+			var newRam = pRam * 2;
+			if (newRam > maxRam) {
+				pRam = maxRam;
+			} else {
+				pRam = newRam;
+			}
 		}
 	}
 }
